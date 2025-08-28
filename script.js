e6d368d76f1cffdec3396d66bd7824d694886399
tyauk <tyauk@bluemountain.ca>
2025-08-28 11:18:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,24 +38,24 @@ function cal_march(){
 
 function showCal(){
   if(pass == 1 && month == 1){
-    document.getElementById("5x7-December").style=.display = "block";
-    document.getElementById("5x7-Janurary").style=.display = "none";
-    document.getElementById("5x7-February").style=.display = "none";
-    document.getElementById("5x7-March").style=.display = "none";
+    document.getElementById("5x7-December").style.display = "block";
+    document.getElementById("5x7-Janurary").style.display = "none";
+    document.getElementById("5x7-February").style.display = "none";
+    document.getElementById("5x7-March").style.display = "none";
   }else if(pass == 1 && month == 2){
-    document.getElementById("5x7-December").style=.display = "none";
-    document.getElementById("5x7-Janurary").style=.display = "block";
-    document.getElementById("5x7-February").style=.display = "none";
-    document.getElementById("5x7-March").style=.display = "none";
+    document.getElementById("5x7-December").style.display = "none";
+    document.getElementById("5x7-Janurary").style.display = "block";
+    document.getElementById("5x7-February").style.display = "none";
+    document.getElementById("5x7-March").style.display = "none";
   }else if(pass == 1 && month == 3){
-    document.getElementById("5x7-December").style=.display = "none";
-    document.getElementById("5x7-Janurary").style=.display = "none";
-    document.getElementById("5x7-February").style=.display = "block";
-    document.getElementById("5x7-March").style=.display = "none";
+    document.getElementById("5x7-December").style.display = "none";
+    document.getElementById("5x7-Janurary").style.display = "none";
+    document.getElementById("5x7-February").style.display = "block";
+    document.getElementById("5x7-March").style.display = "none";
   }else if(pass == 1 && month == 4){
-    document.getElementById("5x7-December").style=.display = "none";
-    document.getElementById("5x7-Janurary").style=.display = "none";
-    document.getElementById("5x7-February").style=.display = "none";
-    document.getElementById("5x7-March").style=.display = "block";
+    document.getElementById("5x7-December").style.display = "none";
+    document.getElementById("5x7-Janurary").style.display = "none";
+    document.getElementById("5x7-February").style.display = "none";
+    document.getElementById("5x7-March").style.display = "block";
   }
 }
