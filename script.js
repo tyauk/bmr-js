67a73e5d666f925da295cd7e8d41ee331676b81c
tyauk <tyauk@bluemountain.ca>
2025-08-28 10:01:10 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function acess5x7(){
+  document.getElementById("5x7-Access").style.display = "block";
+  document.getElementById("5x7XL-Access").style.display = "none";
+  document.getElementById("Ikon-Access").style.display = "none";
+}
+
+function acess5x7xl(){
+  document.getElementById("5x7-Access").style.display = "none";
+  document.getElementById("5x7XL-Access").style.display = "block";
+  document.getElementById("Ikon-Access").style.display = "none";
+}
+
+function acessikon(){
+  document.getElementById("5x7-Access").style.display = "none";
+  document.getElementById("5x7XL-Access").style.display = "none";
+  document.getElementById("Ikon-Access").style.display = "block";
+}
